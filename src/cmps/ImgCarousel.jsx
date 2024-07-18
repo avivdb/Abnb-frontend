232056642ed985229ba00e808f92bd09d600ef23
Mia Darvasi <miadarv@gmail.com>
2024-07-18 11:23:14 +0300

    debug carousel

--- src/cmps/ImgCarousel.jsx
+++ src/cmps/ImgCarousel.jsx
@@ -1,29 +1,26 @@
-import React from "react";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import arrowForward from "../assets/img/icons/arrowforward.svg";
-import arrowBack from "../assets/img/icons/arrowback.svg";
-import { Link } from 'react-router-dom'
-import { useNavigate } from "react-router-dom";
+import React from "react"
+import { Carousel } from "react-responsive-carousel"
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import arrowForward from "../assets/img/icons/arrowforward.svg"
+import arrowBack from "../assets/img/icons/arrowback.svg"
+
 
 export function ImgCarousel({ stay }) {
 
-  const navigate = useNavigate()
   const images = stay.imgUrls;
 
   const handlePrevClick = (e, clickHandler) => {
-    e.stopPropagation(); // Stop propagation to prevent bubbling
-    clickHandler(); // Call the original click handler to navigate the carousel
-  };
+    e.stopPropagation()
+    e.preventDefault()
+    clickHandler()
+  }
 
   const handleNextClick = (e, clickHandler) => {
-    e.stopPropagation(); // Stop propagation to prevent bubbling
-    clickHandler(); // Call the original click handler to navigate the carousel
-  };
-
-  function navigateToDetails() {
-    navigate(`/stay/${stay._id}`)
+    e.stopPropagation()
+    e.preventDefault()
+    clickHandler()
   }
+
 
   return (
     <div className="img-carousel">
@@ -56,12 +53,11 @@ export function ImgCarousel({ stay }) {
         {images.map((URL, index) => (
           <div key={index} className="slide">
               <img 
-              onClick={navigateToDetails}
               alt={`Slide ${index + 1}`} 
               src={URL} />
           </div>
         ))}
       </Carousel>
     </div>
-  );
+  )
 }
--- src/cmps/StayPreview.jsx
+++ src/cmps/StayPreview.jsx
@@ -1,22 +1,22 @@
-// import { Link } from 'react-router-dom'
-// import { ImgCarousel } from './ImgCarousel'
+import { Link } from 'react-router-dom'
+import { ImgCarousel } from './ImgCarousel'
 
 
-// export function StayPreview({ stay }) {
-//     return <Link to={`/stay/${stay._id}`}>
-//         <article className="stay-preview">
-//             <ImgCarousel stay={stay}/>
-//             {/* <img src={stay.imgUrls[0]} /> */}
-//             <section className="stay-preview-top">
-//                 <h2>{stay.loc.city}, {stay.loc.country}</h2>
-//                 <p>&#9733; {stay.rating}</p>
-//             </section>
-//             <p className="secondary-content">1000 kilometers away</p>
-//             <p className="secondary-content">Jan 12-23</p>
-//             <p><span className="stay-preview-price">₪{stay.price}</span> night</p>
-//         </article>
-//     </Link>
-// }
+export function StayPreview({ stay }) {
+    return <Link to={`/stay/${stay._id}`}>
+        <article className="stay-preview">
+            <ImgCarousel stay={stay}/>
+            {/* <img src={stay.imgUrls[0]} /> */}
+            <section className="stay-preview-top">
+                <h2>{stay.loc.city}, {stay.loc.country}</h2>
+                <p>&#9733; {stay.rating}</p>
+            </section>
+            <p className="secondary-content">1000 kilometers away</p>
+            <p className="secondary-content">Jan 12-23</p>
+            <p><span className="stay-preview-price">₪{stay.price}</span> night</p>
+        </article>
+    </Link>
+}
 
 
 
@@ -24,25 +24,25 @@
 // TO TEST CAROUSEL WITHOUT LINK ////////////////////////////
 
 
-import { Link } from 'react-router-dom'
-import { ImgCarousel } from './ImgCarousel'
+// import { Link } from 'react-router-dom'
+// import { ImgCarousel } from './ImgCarousel'
 
 
-export function StayPreview({ stay }) {
-    return <article className="stay-preview">
-        <ImgCarousel stay={stay} />
-        <Link to={`/stay/${stay._id}`}>
-            <section className="stay-preview-details">
-                <section className="flex-column">
-                    <section className="stay-preview-top">
-                        <h2>{stay.loc.city}, {stay.loc.country}</h2>
-                        <p>&#9733; {stay.rating}</p>
-                    </section>
-                    <p className="secondary-content">1000 kilometers away</p>
-                    <p className="secondary-content">Jan 12-23</p>
-                </section>
-                <p><span className="stay-preview-price">₪{stay.price}</span> night</p>
-            </section>
-        </Link>
-    </article>
-}
+// export function StayPreview({ stay }) {
+//     return <article className="stay-preview">
+//         <ImgCarousel stay={stay} />
+//         <Link to={`/stay/${stay._id}`}>
+//             <section className="stay-preview-details">
+//                 <section className="flex-column">
+//                     <section className="stay-preview-top">
+//                         <h2>{stay.loc.city}, {stay.loc.country}</h2>
+//                         <p>&#9733; {stay.rating}</p>
+//                     </section>
+//                     <p className="secondary-content">1000 kilometers away</p>
+//                     <p className="secondary-content">Jan 12-23</p>
+//                 </section>
+//                 <p><span className="stay-preview-price">₪{stay.price}</span> night</p>
+//             </section>
+//         </Link>
+//     </article>
+// }
