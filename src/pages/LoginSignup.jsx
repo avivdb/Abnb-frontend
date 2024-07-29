d7b36d8e36bcf5cf3b86c9dd185cd04145cee730
Aviv Baron David <bardaviv@gmail.com>
2024-07-29 19:11:53 +0300

    Log in with google account

--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -1,10 +1,14 @@
-import { Outlet } from 'react-router'
-import { NavLink } from 'react-router-dom'
+import { Outlet } from 'react-router';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const clientId = '645691889779-la8vv9598g6t4qhg7k12quiil6cqa4uq.apps.googleusercontent.com'; // Replace with your actual Google Client ID
 
 export function LoginSignup() {
     return (
-        <div className="login-page">
-            <Outlet/>
-        </div>
-    )
+        <GoogleOAuthProvider clientId={clientId}>
+            <div className="login-page">
+                <Outlet />
+            </div>
+        </GoogleOAuthProvider>
+    );
 }
